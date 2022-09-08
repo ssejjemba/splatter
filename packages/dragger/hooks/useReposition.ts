c728b81ae7adcf258504fa1b7c128a2605a68251
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-09-08 17:26:17 +0300

    Add hook for handling repositioning of elements
    This returns the left and top properties that should be applied to an element whose ref is supplied as input
    
    It also takes defaults to account for persistence

---
+++ packages/dragger/hooks/useReposition.ts
@@ -0,0 +1,39 @@
+// This will use the drag deltas to return a new position (x, y) for an element
+
+import { useState } from "react";
+
+import { DragEventData } from "../models";
+import { useDrag } from "./useDrag";
+
+export function useReposition(
+  defaults: { left: number; top: number },
+  elementRef: React.RefObject<HTMLElement>
+): { left: number; top: number; isRepositioning: boolean } {
+  const [left, setLeft] = useState(defaults.left);
+  const [top, setTop] = useState(defaults.top);
+
+  const onDragStart = (event: DragEventData) => {
+    console.log("starting to reposition the block");
+  };
+
+  const onDragMove = (event: DragEventData) => {
+    setLeft(left + event.deltaX);
+    setTop(top + event.deltaY);
+  };
+
+  const onDragEnd = (event: DragEventData) => {
+    console.log("stopped repositioning the block");
+  };
+
+  const { isDragging } = useDrag(elementRef, {
+    onDragStart,
+    onDragMove,
+    onDragEnd,
+  });
+
+  return {
+    left,
+    top,
+    isRepositioning: isDragging,
+  };
+}
