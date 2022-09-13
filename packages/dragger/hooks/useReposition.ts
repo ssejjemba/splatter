c70c328768c5a8d2df583e185b3c71abf298434a
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-09-13 19:21:07 +0300

    (Fix): Update the dragged element to always be on the top stack
    - This takes the assumption that user doesn't manually assign draggable element an index bigger than 9999

--- packages/dragger/hooks/useReposition.ts
+++ packages/dragger/hooks/useReposition.ts
@@ -1,6 +1,6 @@
 // This will use the drag deltas to return a new position (x, y) for an element
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { DragEventData } from "../models";
 import { useDrag } from "./useDrag";
@@ -17,6 +17,8 @@ export function useReposition(
   const [left, setLeft] = useState(defaults.left);
   const [top, setTop] = useState(defaults.top);
 
+  const originalStackIndex = useRef<string>("auto");
+
   const setDimensions = (newDimens: { left: number; top: number }) => {
     setLeft(newDimens.left);
     setTop(newDimens.top);
@@ -24,6 +26,9 @@ export function useReposition(
 
   const onDragStart = (event: DragEventData) => {
     console.log("starting to reposition the block");
+    if (elementRef && elementRef.current) {
+      elementRef.current.style.zIndex = "9999";
+    }
   };
 
   const onDragMove = (event: DragEventData) => {
@@ -33,7 +38,16 @@ export function useReposition(
 
   const onDragEnd = (event: DragEventData) => {
     console.log("stopped repositioning the block");
+    if (elementRef && elementRef.current) {
+      elementRef.current.style.zIndex = originalStackIndex.current;
+    }
   };
+
+  useEffect(() => {
+    if (elementRef && elementRef.current) {
+      originalStackIndex.current = elementRef.current.style.zIndex;
+    }
+  }, [elementRef]);
 
   const { isDragging } = useDrag(elementRef, {
     onDragStart,
