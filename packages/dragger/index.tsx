b24f3bb634aa1a4925dbc7bf69328e557b007fba
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-09-15 19:29:21 +0300

    (Feat): Export events hooks from the dragger.
    - These hooks can be reused in the selector module

--- packages/dragger/index.tsx
+++ packages/dragger/index.tsx
@@ -1,5 +1,13 @@
 import { useReposition } from "./hooks/useReposition";
 import { Resizer } from "./components/Resizer";
 import { DraggableElement } from "./components/DraggableElement";
+import useMouseEvents from "./hooks/useMouseEvents";
+import useTouchEvents from "./hooks/useTouchEvents";
 
-export { useReposition, Resizer, DraggableElement };
+export {
+  useReposition,
+  Resizer,
+  DraggableElement,
+  useMouseEvents,
+  useTouchEvents,
+};
